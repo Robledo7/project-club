f181feec9b1c5dd0bce286fe843a3174a952b825
Robledo7 <lurobledo.scl@gmail.com>
2024-10-26 22:23:56 -0300

    Tercer test-combinado el Fullmember y Semimember

--- test.php
+++ test.php
@@ -7,8 +7,7 @@ include_once 'semipleno.php';
 /**$nuevoSocio=new Member(123);
 echo $nuevoSocio->calculateAge('2004-07-16');*/
 
-$valorCuota=new FullMember(123);
-echo $valorCuota->calcularCuota('2004-07-16');
-
-$valorCuota=new SemiMember(456);
-echo $valorCuota->calcularCuota('2004-07-16');
+$members=[new FullMember(123), new SemiMember(456)];
+foreach ($members as $member){
+    echo $member->calcularCuota("2022-08-06")."\n";
+}
