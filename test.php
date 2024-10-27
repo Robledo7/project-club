ccfd499ecc4779c0a64172815f123be9be3d95ab
Robledo7 <lurobledo.scl@gmail.com>
2024-10-26 22:21:22 -0300

    Segundo test-agragado el semimember

--- test.php
+++ test.php
@@ -2,9 +2,13 @@
 
 include_once 'member.php';
 include_once 'pleno.php';
+include_once 'semipleno.php';
 
-$nuevoSocio=new Member(123);
-echo $nuevoSocio->calculateAge('2004-07-16');
+/**$nuevoSocio=new Member(123);
+echo $nuevoSocio->calculateAge('2004-07-16');*/
 
 $valorCuota=new FullMember(123);
+echo $valorCuota->calcularCuota('2004-07-16');
+
+$valorCuota=new SemiMember(456);
 echo $valorCuota->calcularCuota('2004-07-16');
